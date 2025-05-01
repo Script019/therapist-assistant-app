5c0f3561f7ed8c37f0a417bd12b35fce6067dacc
Script019 <andrew.schuck@proton.me>
2025-04-30 20:52:46 -0400

    Final sync with Visual Studio Code

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,10 @@
-import tailwindcss from '@tailwindcss/postcss';
+import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 
 /** @type {import('postcss').Config} */
 export default {
   plugins: [
     tailwindcss,
-    autoprefixer,
+    autoprefixer
   ]
 };
