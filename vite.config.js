b2bd4da4638f3026e2a7115ed111c91b5c5270db
Script019 <andrew.schuck@proton.me>
2025-05-01 15:44:18 -0400

    Rebuild before deploy to fix 404

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/therapist-assistant-app/', // This must match your repo name!
+  base: '/therapist-assistant-app/', // ✅ Must match repo name
   plugins: [react()],
 });
